a510a99ee56e6be44bad7e9fc34300f3659d22cc
NanaSpace <zhuwentianzz@gmail.com>
2024-08-06 16:28:19 -0400

    WIP

---
+++ Assets/Needle/Components.codegen/ChaDragState.cs
@@ -0,0 +1,19 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class ChaDragState : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.Animator @chaAnimator;
+		public void awake(){}
+		public void start(){}
+		public void onPointerDown(UnityEngine.EventSystems.PointerEventData @args){}
+		public void onPointerUp(UnityEngine.EventSystems.PointerEventData @args){}
+		public void update(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
